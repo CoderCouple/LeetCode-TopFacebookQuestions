5e2d6e6c350409cac99b2cc7409d7e0ab1ecaa43
Sunil Tiwari <sunil28071987@gmail.com>
2018-09-18 19:47:06 -0700

    Update CelebritySearch.java

--- CelebritySearch.java
+++ CelebritySearch.java
@@ -3,7 +3,7 @@ package FBQuestions;
 /**
  * QUESTION:
  * =========
- *Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity. The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
+ * Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity. The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
  * Now you want to find out who the celebrity is or verify that there is not one. The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
  * You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n), your function should minimize the number of calls to knows.
  *
@@ -52,11 +52,6 @@ public class CelebritySearch {
                 if(individual>celebrity && !knows(individual,celebrity))
                     return -1;
             }
-
             return celebrity;
-        }
-
-        public boolean knows(int a , int b){
-            return true;
         }
 }
